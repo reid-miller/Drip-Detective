6e1b50771b5b84b0ec480646062338ceb865dcd1
MarkT <50853333+mtru32@users.noreply.github.com>
2019-10-19 18:52:15 -0500

    Script and HTML
    Changes made for site

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 
 var minutes = 0;
-var seconds = 01;
+var seconds = 15;
+var btn = document.createElement("BUTTON");
+btn.innerHTML = "Stop Timer";
+// document.body.appendChild(btn);
+
 function startTime() {
     document.getElementById("timer").innerHTML = minutes + ":" + seconds;
 }
 
-//function
+function timer() {
+
+}

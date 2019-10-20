4e76561ac26365e94e48f4480cc05ce85a7cacd5
MarkT <50853333+mtru32@users.noreply.github.com>
2019-10-20 01:36:26 -0500

    Added statistics function for results

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 var minutes = 0;
 var seconds = 00;
 var i = 0;
+var galPerSec = .035;
+var dollarPerGal = .03;
+
 var timeStarter = 0; // 0 means timer hasn't started 
                     // 1 means timer has been pressed and is running
                     // 2 means timer has been pressed again and should be stopped
@@ -26,6 +29,7 @@ function update() {
         document.getElementById("timer").innerHTML = pad(minutes) + ":" + pad(seconds);
     } else if (timeStarter == 2){
         document.getElementById("start-stop").style.visibility = 'hidden';
+        statistics();
     }
 }
 
@@ -40,4 +44,12 @@ function add() {
 
 function pad(n) {
     return (n < 10) ? ("0" + n) : n;
+}
+
+function statistics() {
+    var totalGal = ((minutes * 60) + seconds) * galPerSec;
+    document.getElementById("stat-timer").innerHTML = pad(minutes) + ":" + pad(seconds);
+    document.getElementById("num-gallons").innerHTML = totalGal.toFixed(2);
+    document.getElementById("bill-impact").innerHTML = (totalGal * dollarPerGal).toFixed(2);
+    document.getElementById("total-bill").innerHTML = (totalGal * dollarPerGal * 30).toFixed(2);
 }

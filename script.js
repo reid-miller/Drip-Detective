de783c564095bb9f61fe7d30131b17eea0dec812
MarkT <50853333+mtru32@users.noreply.github.com>
2019-10-19 22:14:37 -0500

    Timer
    Timer iteration running smoothly
    //FIXME stop button

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 
 var minutes = 0;
-var seconds = 15;
-var btn = document.createElement("BUTTON");
-btn.innerHTML = "Stop Timer";
-// document.body.appendChild(btn);
+var seconds = 00;
+var i = 0;
+var timeStarter = 0; // 0 means timer hasn't started 
+                    // 1 means timer has been pressed and is running
+                    // 2 means timer has been pressed again and should be stopped
 
-function startTime() {
-    document.getElementById("timer").innerHTML = minutes + ":" + seconds;
-    document.getElementById("start-shower").childNodes[0].innerHTML = "Stop Timer";
+function timeButton() {
+    if (timeStarter == 0) {
+        timeStarter = 1;
+        document.getElementById("start-stop").childNodes[0].innerHTML = "Stop Timer";
+        timer();
+    } else if (timeStarter == 1) {
+        timeStarter = 2;
+    }
 }
 
 function timer() {
+   if (timeStarter == 1) {
+        setInterval(update, 1000);
+   }
+}
 
+function update() {
+    add();
+    document.getElementById("timer").innerHTML = minutes + ":" + seconds;
+}
+
+function add() {
+    if (seconds >= 59) {
+        seconds = 0;
+        minutes = minutes + 1;
+    } else {
+        seconds = seconds + 1;
+    }
 }
